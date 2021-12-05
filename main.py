0cd00d87698c7b4a67ab29b775746ef9e03d3b5a
sreevishnu653 <sreevishnu653@gmail.com>
2021-12-05 14:37:54 +0530

    new files added

---
+++ main.py
@@ -0,0 +1,15 @@
+# i = 1
+# num = int(input("Enter any number  : "))
+# while i <= 10:
+#     print(num," * ",i," = ", num * i)
+#     i = i+1
+# print("Welcome to multiplication by 2")
+# i=1
+# num=int(input("Enter any number: "))
+# while i<=10:
+#     print ("Answer is ",num,"*",i,"=", num*i)
+#     i=i+1
+
+
+
+# print("hello world")
